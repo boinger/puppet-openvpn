63994552245e68109abac27cd634c61f7dd31f71
jeff@jeffvier.com <jeff@jeffvier.com>
2013-08-27 16:06:17 -0700

    still fighting with user var

--- manifests/service.pp
+++ manifests/service.pp
@@ -30,7 +30,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-define openvpn::service(
+define openvpn::service (
   $serviceprovider = 'daemontools'
 ){
   if ($serviceprovider == "daemontools" ) {
