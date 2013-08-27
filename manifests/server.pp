595ea13b67dba164b5f960f75fe1bc7a56ce58d2
jv <jeff@jeffvier.com>
2013-08-27 11:02:07 -0700

    change default protocol to udp (tcp over tcp is terrible)

--- manifests/server.pp
+++ manifests/server.pp
@@ -59,7 +59,7 @@
 #
 # [*proto*]
 #   String.  What IP protocol is being used.
-#   Default: tcp
+#   Default: udp
 #   Options: tcp or udp
 #
 # [*status_log*]
@@ -125,7 +125,7 @@ define openvpn::server(
   $local = $::ipaddress_eth0,
   $logfile = false,
   $port = '1194',
-  $proto = 'tcp',
+  $proto = 'udp',
   $status_log = "${name}/openvpn-status.log",
   $server = '',
   $push = []
